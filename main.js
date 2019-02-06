d4655e800df4c7089bd982f4c0354140b0da0e5f
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-02-06 12:45:04 -0500

    fixed up flex issues by adjusting content height. Also touched up general layout. Slideshow in basic function before buttons

--- main.js
+++ main.js
@@ -20,5 +20,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += "active";
-  setTimeout(showSlides, 8000);
+  setTimeout(showSlides, 5000);
 }
